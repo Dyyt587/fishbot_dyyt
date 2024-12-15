3a147b3c974d76fe783143a02729c38fca777742
Dyyt587 <67887002+Dyyt587@users.noreply.github.com>
2024-12-15 17:24:26 +0800

    Initial commit

---
+++ scripts/control.sh
@@ -0,0 +1,3 @@
+ 
+source install/setup.bash
+ros2 run teleop_twist_keyboard teleop_twist_keyboard 
---
+++ scripts/save_map.sh
@@ -0,0 +1,2 @@
+cd src/fishbot_cartographer/ && mkdir map && cd map
+ros2 run nav2_map_server map_saver_cli -t map -f fishbot_map
